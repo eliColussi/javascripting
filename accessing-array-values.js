d75f614bc06f770e446dbd2d29d0186744001bb6
eli <eliitheboss@gmail.com>
2022-09-08 06:02:56 +0000

    object practice

--- accessing-array-values.js
+++ accessing-array-values.js
@@ -1,3 +1,27 @@
 const food = ['apple', 'pizza', 'pear']
 
 console.log(food[1])
+
+
+
+const finalPosition = function (moves) {
+  // Your code in here ...
+  var position = [0,0];
+  for (var move of moves) {
+    switch(move) {
+      case('north'):
+        position[1]++;
+        break;
+      case('south'):
+        position[1]--;
+        break;
+      case('east'):
+        position[0]++;
+        break;
+      case('west'):
+        position[0]--;
+        break;
+    }
+  }
+  return position
+;};
